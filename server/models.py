cfa7dc1a22737c7efc635ada08733e356c1a518d
steve-the-hermit <stevemacharia309@gmail.com>
2023-10-08 13:22:37 +0300

    done

--- server/models.py
+++ server/models.py
@@ -7,3 +7,7 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
